24f01545dd079a64ba54186f229774fbec0d2bdc
n4n0 <n4n0@tuta.io>
2023-11-10 16:13:05 +0100

    ffffffffffffffff

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# yt-embed
-Because fuck YouTube, that's why.
+fuck youtube's anti-ad-block bullshit
+
+copy-pasta the URL from a youtube video you want to watch and paste it into the input field of this embedding thing and enjoy.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Youtube from 'svelte-youtube-embed'
+  let videoIdRaw = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  $: videoIdSanitized = new URL(videoIdRaw).searchParams.get('v')
+
+</script>
+
+<p>Paste a YouTube URL into the input field and play the video through an embedded version that respects your ad-blocker.</p>
+<input type="text" bind:value={videoIdRaw} style="height:30px; width:100%;">
+{#if videoIdSanitized !== '' && videoIdSanitized !== null}
+  <Youtube id="{videoIdSanitized}" />
+{/if}
